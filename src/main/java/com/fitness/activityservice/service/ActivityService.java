ca7a82b569e5ff1a9f98e8c76d43cf72e2231bbe
Taiwo Olapade <semiteprofessor@gmail.com>
2026-03-10 18:07:07 +0100

    Commit

---
+++ src/main/java/com/fitness/activityservice/service/ActivityService.java
@@ -0,0 +1,11 @@
+package com.fitness.activityservice.service;
+
+import com.fitness.activityservice.dto.ActivityRequest;
+import com.fitness.activityservice.dto.ActivityResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ActivityService {
+    public ActivityResponse trackActivity(ActivityRequest request) {
+    }
+}
