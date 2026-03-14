e23b8df0cdef8d78635a1097e82875168e0b5a20
Taiwo Olapade <semiteprofessor@gmail.com>
2026-03-14 18:47:31 +0100

    Commit

--- src/main/java/com/fitness/activityservice/controller/ActivityController.java
+++ src/main/java/com/fitness/activityservice/controller/ActivityController.java
@@ -16,13 +16,18 @@ public class ActivityController {
 
     private ActivityService activityService;
 
-    @PostMapping("/register")
+    @PostMapping
     public ResponseEntity<ActivityResponse> trackActivity(@RequestBody ActivityRequest request) {
         return ResponseEntity.ok(activityService.trackActivity(request));
     }
 
-    @PostMapping("/register")
+    @GetMapping
     public ResponseEntity<List<ActivityResponse>> getUserActivities(@RequestHeader("X-User-ID") String userId){
         return ResponseEntity.ok(activityService.getUserActivities(userId));
+    }
+
+    @GetMapping("/{activityId}")
+    public ResponseEntity<ActivityResponse> getActivity(@PathVariable String activityId){
+        return ResponseEntity.ok(activityService.getActivity(activityId));
     }
 }
