677223cae4dbb774161c3939b6a6a1861c49ce67
Taiwo Olapade <semiteprofessor@gmail.com>
2026-04-16 09:20:18 -0700

    Commit

--- src/main/java/com/fitness/activityservice/service/UserValidationService.java
+++ src/main/java/com/fitness/activityservice/service/UserValidationService.java
@@ -11,24 +11,33 @@ import org.springframework.web.reactive.function.client.WebClientResponseExcepti
 @Slf4j
 @RequiredArgsConstructor
 public class UserValidationService {
+
     private final WebClient userServiceWebClient;
 
     public boolean validateUser(String userId) {
         log.info("Calling User Validation API for userId: {}", userId);
-        try {
-            return Boolean.TRUE.equals(userServiceWebClient.get()
-                    .uri("/api/users/{userId}/validate", userId)
-                    .retrieve()
-                    .bodyToMono(Boolean.class)
-                    .block());
-            } catch (WebClientResponseException e) {
-            if (e.getStatusCode() == HttpStatus.NOT_FOUND)
-                throw new RuntimeException("User not Found:" + userId);
-            else if (e.getStatusCode() == HttpStatus.BAD_REQUEST) {
-                throw new RuntimeException("Invalid Request: " + userId);
-            }
-            return  false;
-        }
 
+        try {
+            return Boolean.TRUE.equals(
+                    userServiceWebClient.get()
+                            .uri("/api/users/{userId}/validate", userId)
+                            .retrieve()
+                            .bodyToMono(Boolean.class)
+                            .block()
+            );
+
+        } catch (WebClientResponseException e) {
+
+            if (e.getStatusCode() == HttpStatus.NOT_FOUND) {
+                throw new RuntimeException("User not found: " + userId);
+            }
+
+            if (e.getStatusCode() == HttpStatus.BAD_REQUEST) {
+                throw new RuntimeException("Invalid request for user: " + userId);
+            }
+
+            log.error("Error calling User Service", e);
+            return false;
+        }
     }
 }
